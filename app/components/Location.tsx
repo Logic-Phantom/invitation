0d52aa7ece0d08b3289de811b969685c1dbf8ecd
dlacoaud92 <dlacoaud92@naver.com>
2025-05-28 15:03:10 +0900

    [임채명] 각 교통수단에 맞게 변경

--- app/components/Location.tsx
+++ app/components/Location.tsx
@@ -143,9 +143,9 @@ const Location = () => {
           currentPolyline.setMap(null);
         }
 
-        const routeType = selectedRouteType === 'walk' ? 'walk' : 
-                        selectedRouteType === 'bus' ? 'transit' : 'car';
-                        const url = `https://map.kakao.com/link/to/경복궁,${endLat},${endLng}?mode=${routeType}&from=${startLat},${startLng}`;
+        const routeType = selectedRouteType === 'walk' ? 'pedestrian' : 
+                        selectedRouteType === 'bus' ? 'public' : 'car';
+        const url = `https://map.kakao.com/link/to/경복궁,${endLat},${endLng}?mode=${routeType}&from=${startLat},${startLng}`;
         window.open(url, '_blank');
 
         const polyline = new window.kakao.maps.Polyline({
